2ff0a2480c99cadf9e2f14d74d08db88cec12805
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2024-12-08 23:30:15 +0200

    recent updates

--- rock-paper-scissors/src/game.py
+++ rock-paper-scissors/src/game.py
@@ -1,30 +1,49 @@
 import random
-#game logic
-options = ("rock", "paper","scissors")
-player = None
-computer = random.choice(options)
+# game logic
+options = ("rock", "paper", "scissors")
+running = True
 
-while player not in options:
-    player = input("Enter a choice(rock, paper, scissors): ")
+while running:
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"player: {player}")
+    print(f"computer: {computer}")
+
+    # win conditions
+    if player == computer:
+        print("It's a Tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You Win!")
+    elif player == "paper" and computer == "rock":
+        print("You Win!")
+    elif player == "scissors" and computer == "paper":
+        print("You Win!")
+    else:
+        print("You lose!")
+
+    play_again = input("play again? (y/n): ").lower()
+    if not play_again == "y":
+        running = False  
+
+
+print("thanks for playing")
 
 
 
-print(f"player:{player}")
-print(f"computer:{computer}")
-
-
-## win conditions
-if player == computer:
-    print("It's a Tie!")
-elif player == "rock" and computer == "scissors":
- print("You Win!'")
-elif player == "paper" and computer == "rock":
-    print("You Win!")
-elif player == "scissors" and computer =="paper":
-    print("You Win!")
-else:
-     print("You lose!")    
 
 ## open the venv: source venv/bin/activate
 ##  cd /Users/noah/rock-paper-scissors/src
 ## python3 game.py
+
+
+
+
+
+## pushing changes to GitHub:
+## git add game.py
+## git commit -m "update game.py"
+## git push origin main
